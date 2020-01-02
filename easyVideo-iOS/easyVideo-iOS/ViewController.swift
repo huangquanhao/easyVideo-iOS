4bbb7952117623cd7d41246e6bf107cc4ba21d2b
huangquanhao <641224997@qq.com>
2020-01-02 11:12:00 +0800

    add cocopods.

---
+++ easyVideo-iOS/easyVideo-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easyVideo-iOS
+//
+//  Created by quanhao huang on 2020/1/2.
+//  Copyright © 2020 quanhao huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
